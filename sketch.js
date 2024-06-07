1ff0d3f80ace437b6de93d606a3624b3256982be
juninho011 <dirceujunior9812@gmail.com>
2024-06-07 09:06:58 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  //cria uma tela de 400px de largura por 400px de altura 
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("black");
+  
+ stroke("red");
+  fill("white");
+  if (mouseIsPressed);
+  rect(mouseX,mouseY,20,35);
+}
+
+
+
+
